89d12b590121a7a616a0da84e07dd6edfa88c0ec
React Lab Files <95652257+reactfiles@users.noreply.github.com>
2024-06-18 18:27:22 +0530

    Add files via upload

--- day1/modules/adder.js
+++ day1/modules/adder.js
@@ -4,6 +4,9 @@ export function adder(num1, num2){
     return num1 + num2;
 }; 
 */
+
+import Pson from "./person.js";
+
 function adder(num1, num2){
     return num1 + num2;
 }; 
@@ -12,19 +15,11 @@ function multiplier(num1, num2){
     return num1 * num2;
 }; 
 
-class Person{
-    ability = "i can walk";
-    constructor(nability){
-        this.ability = nability;
-    }
-    sayability(){
-        return this.ability;
-    }
-}
 
 // alias export
 // export { adder as sum }
 
 // named export
-export { adder, multiplier, Person }
+export default adder;
+export { multiplier, Pson }
 
